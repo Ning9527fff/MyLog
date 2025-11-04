66d73a06b93b31931295e989ac6a44eb30683382
mac <mac@Nings-Super-Mac.local>
2025-11-04 12:14:14 +0800

    base

---
+++ log_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func TestLog() {
+
+	testMessage := "Test Message"
+
+	SetLogLevel(DebugLevel)
+	DebugF("This is a debug log")
+	InfoF("This is a info log")
+	ErrorF("This is a error log")
+	NormalF("This is a normal log")
+	DebugF("This is a debug log: %s", testMessage)
+
+	fmt.Println("====================================")
+
+	SetLogLevel(InfoLevel)
+	DebugF("This is a debug log")
+	InfoF("This is a info log")
+	ErrorF("This is a error log")
+	NormalF("This is a normal log")
+	InfoF("This is a info log: %s", testMessage)
+	fmt.Println("====================================")
+
+	SetLogLevel(ErrorLevel)
+	DebugF("This is a debug log")
+	InfoF("This is a info log")
+	ErrorF("This is a error log")
+	NormalF("This is a normal log")
+	ErrorF("This is a error log: %s", testMessage)
+	fmt.Println("====================================")
+
+	SetLogLevel(NormalLevel)
+	DebugF("This is a debug log")
+	InfoF("This is a info log")
+	ErrorF("This is a error log")
+	NormalF("This is a normal log")
+	NormalF("This is a normal log: %s", testMessage)
+	fmt.Println("====================================")
+}
